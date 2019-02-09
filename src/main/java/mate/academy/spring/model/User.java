a658e7c84d1fffa87b6fdef52fb0748c2d9566cb
Illia1927 <edcwsxqaz22>
2019-02-09 11:25:54 +0200

    add some functionality

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,35 @@
+package mate.academy.spring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Builder
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
